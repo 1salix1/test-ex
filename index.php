eb0dba22c0b3de83b32584f7459f30c4503251bd
1salix1 <aleks5758@yandex.ru>
2018-11-24 17:31:49 +0300

    on index error

--- index.php
+++ index.php
@@ -8,6 +8,21 @@
 ?>
 <html>
 <body>
-test
+<?php
+if (isset($_SESSION['error']))
+{
+    ?>
+    <div style="color:red">
+    <?php echo $_SESSION['error']; ?>
+    </div>
+<?php
+    unset($_SESSION['error']);
+}
+?>
+<form action="/action.php" method="post">
+<input type="tel" name="one"><br>
+<input type="tel" name="two"><br>
+    <input type="submit" value="calculate">
+</form>
 </body>
 </html>>
