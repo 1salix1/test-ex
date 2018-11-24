a9bfd27bbf3536311e1574a14da9cbd5836e62d5
1salix1 <aleks5758@yandex.ru>
2018-11-24 18:36:44 +0300

    fix action

--- action.php
+++ action.php
@@ -11,10 +11,10 @@ if (isset($_POST['one']) and (isset($_POST['two'])))
         if ($_POST['one']!="" and $_POST['two']!="")
         {
             $result=$_POST['one']+$_POST['two'];
-            header("/result.php?result=".$result);
+            header("Location: /result.php?result=".$result);
             exit();
         }
         else
         {$_SESSION['error']="input value was empty";}
     }
-header("/index.php");
+header("Location: /index.php");
